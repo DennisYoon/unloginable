06ea8cfbb374915d7fe8d45b802d20145443aa51
DennisYun <2008dennisyun@gmail.com>
2022-08-27 18:38:40 +0900

    added wrong id pw animation

--- src/typescript/dom.ts
+++ src/typescript/dom.ts
@@ -1,4 +1,16 @@
 type HTML = HTMLElement | null;
-const form: HTML = document.querySelector("form");
+type Input = HTMLInputElement | null;
 
-export {form};
+const box: HTML = document.querySelector("#box");
+
+const form: HTML = document.querySelector("form");
+const mkAccount: HTML = document.querySelector("#mkaccount span");
+const findIdPw: HTML = document.querySelector("#find_id_pw span");
+const inputProblem: HTML = document.querySelector("#inputProblem");
+
+const idBox: Input = document.querySelector("#id");
+const pwBox: Input = document.querySelector("#pw");
+
+export {
+  box, form, mkAccount, findIdPw, idBox, pwBox, inputProblem
+};
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,4 +1,8 @@
-import { form } from "./dom";
+import {
+  box,
+  form, mkAccount, findIdPw,
+  idBox, pwBox, inputProblem
+} from "./dom";
 
 declare global {
   interface Window {
@@ -7,9 +11,24 @@ declare global {
 }
 
 window.dologin = function () {
-  console.log("Hello world");
+  inputProblem?.classList.remove("hide");
+  inputProblem?.classList.add("show");
+
+  box?.classList.remove("shake");
+  box?.classList.add("shake");
+
+  idBox ? idBox.value = "" : null;
+  pwBox ? pwBox.value = "" : null;
 }
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
 });
+
+mkAccount?.addEventListener("click", () => {
+  alert("계정 만드는 창으로 넘어가라!");
+});
+
+findIdPw?.addEventListener("click", () => {
+  alert("진정하시고 심호흡 하세요. 당신의 아이디 또는 비밀번호를 기억하려 애쓰세요. 당신의 전화번호나 다른 플렛폼의 계정을 떠올려 보세요!");
+})
